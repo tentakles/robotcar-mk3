dff44540f63c553bf87bdf50fcda23ce1cf9da43
tentakles <ollebergling@gmail.com>
2015-07-31 01:00:05 +0200

    simple tank control

--- code/RoboCar/RoboCarModel1.py
+++ code/RoboCar/RoboCarModel1.py
@@ -1,11 +1,7 @@
-import logging, sys
+import logging, sys, time, atexit
 from RoboCar import RoboCarBase
-
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 from Adafruit_PWM_Servo_Driver import PWM
-
-import time
-import atexit
 
 #implementation for first type of car
 class RoboCarModel1(RoboCarBase.RoboCarBase):
@@ -16,10 +12,10 @@ class RoboCarModel1(RoboCarBase.RoboCarBase):
         self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
         self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
 
-    def __init__(self,i2c_addr):
+    def __init__(self,i2c_addr):  
         super(RoboCarModel1,self).__init__()
         #logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-        self.i2c_addr = i2c_addr;
+        self.i2c_addr = i2c_addr
         self.mh = Adafruit_MotorHAT(addr=i2c_addr)
         self.pwm = PWM(0x60)
         self.servoMin = 150  # Min pulse length out of 4096
@@ -28,13 +24,16 @@ class RoboCarModel1(RoboCarBase.RoboCarBase):
         self.motorLeft = self.mh.getMotor(3)
         self.motorRight = self.mh.getMotor(1)
         atexit.register(self.stop)
+        self.isRunningForward=0
+        self.isRunningReverse=0
+        self.isRunningLeft=0
+        self.isRunningRight=0
 
     def setCamAngle(self,angle):
         self.camAngle=angle;
         newAngle = self.map(self.camAngle,0,180,self.servoMin,self.servoMax)
         self.pwm.setPWM(0, 0, newAngle)
-        #logging.debug("setting camera angle: {}".format(self.camAngle))  
-      
+
     def setMotors(self,motorLeftSpeed,motorRightSpeed,motorLeftDir,motorRightDir):
         # set the speed to start, from 0 (off) to 255 (max speed)
         newLeftSpeed = self.map(motorLeftSpeed,0,10,0,255)
@@ -45,21 +44,43 @@ class RoboCarModel1(RoboCarBase.RoboCarBase):
         self.motorRight.run(motorRightDir)
        
     def setMotorForward(self):
+        self.isRunningForward=1
+        self.isRunningReverse=0
         self.setMotors(self.motorSpeed,self.motorSpeed,Adafruit_MotorHAT.FORWARD,Adafruit_MotorHAT.FORWARD)
      
     def setMotorReverse(self):
+        self.isRunningForward=0
+        self.isRunningReverse=1
         self.setMotors(self.motorSpeed,self.motorSpeed,Adafruit_MotorHAT.BACKWARD,Adafruit_MotorHAT.BACKWARD)
         
     def setMotorForwardReverseStop(self):
-        self.stop()
+        self.isRunningForward=0
+        self.isRunningReverse=0  
+        if(self.isRunningLeft):
+            self.setMotorLeft()
+        elif(self.isRunningRight):
+            self.setMotorRight()
+        else:
+            self.stop()
         
     def setMotorLeftRightStop(self):
-        self.stop()
+        self.isRunningLeft=0
+        self.isRunningRight=0
+        if(self.isRunningForward):
+            self.setMotorForward()
+        elif(self.isRunningReverse):
+            self.setMotorReverse()
+        else:
+            self.stop()
         
     def setMotorLeft(self):
+        self.isRunningLeft=1
+        self.isRunningRight=0
         self.setMotors(self.motorTurnSpeed,self.motorTurnSpeed,Adafruit_MotorHAT.BACKWARD,Adafruit_MotorHAT.FORWARD)
         
     def setMotorRight(self):
+        self.isRunningLeft=0
+        self.isRunningRight=1
         self.setMotors(self.motorTurnSpeed,self.motorTurnSpeed,Adafruit_MotorHAT.FORWARD,Adafruit_MotorHAT.BACKWARD)
         
      
